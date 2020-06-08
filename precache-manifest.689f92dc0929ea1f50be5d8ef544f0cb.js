9f34a3ee07675e520d1cd0fd6f5ca795271bb23e
TonyMakis <ajmakis@gmail.com>
2020-06-08 09:29:17 -0500

    Updates

---
+++ precache-manifest.689f92dc0929ea1f50be5d8ef544f0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a15c950bc243f82e09be7a890dc859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d548e5155a78b78df04e",
+    "url": "/static/css/main.6b4087b8.chunk.css"
+  },
+  {
+    "revision": "65e8153d6ff00196158a",
+    "url": "/static/js/2.335b575e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.335b575e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d548e5155a78b78df04e",
+    "url": "/static/js/main.816bf268.chunk.js"
+  },
+  {
+    "revision": "47b4946ac72d88ddd3c2",
+    "url": "/static/js/runtime-main.55cbc656.js"
+  }
+]);
