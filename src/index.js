71b2f49553c07e925e289a10a14af9c874a4138a
Tony Makis <ajmakis@gmail.com>
2021-01-22 17:12:19 -0600

    Reverting service worker, that failed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from `${process.env.PUBLIC_URL}/serviceWorker`;
+import * as serviceWorker from `./serviceWorker`;
 
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
