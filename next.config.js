e1775ee91e8ae04554e54c4fd1e83f0be1f3b406
TonyMakis <tonymakis@protonmail.com>
2025-10-24 13:23:28 -0500

    Simplify Next.js config - remove basePath for GitHub Pages compatibility

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/TonyMakis.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/TonyMakis.github.io' : '',
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.publicPath = process.env.NODE_ENV === 'production' ? '/TonyMakis.github.io/assets/' : '/_next/static/';
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
